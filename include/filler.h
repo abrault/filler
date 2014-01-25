a98caa6ae7ed0dadb1e0c8bb5184021b7563937a
abrault <abrault@student.42.fr>
2014-01-25 20:44:45 +0100

    recuperation de donne ok

--- include/filler.h
+++ include/filler.h
@@ -6,7 +6,7 @@
 /*   By: abrault <abrault@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/01/24 16:32:09 by abrault           #+#    #+#             */
-/*   Updated: 2014/01/24 17:25:57 by abrault          ###   ########.fr       */
+/*   Updated: 2014/01/25 17:10:33 by abrault          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,14 @@
 # define FILLER_H
 
 typedef struct s_map	t_map;
+typedef struct s_piece	t_piece;
+
+struct					s_piece
+{
+	int					width;
+	int					height;
+	char				**piece;
+};
 
 struct					s_map
 {
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: abrault <abrault@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/01/24 16:28:54 by abrault           #+#    #+#             */
-/*   Updated: 2014/01/24 17:25:55 by abrault          ###   ########.fr       */
+/*   Updated: 2014/01/25 19:04:44 by abrault          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,126 @@
 void	get_dim_map(t_map *map)
 {
 	char	*line;
+	char	*dim;
 
 	line = NULL;
-	get_next_line(1, &line);
-	(void)map;
-	write(1, "Recu :", 6);
-	write(2, line, ft_strlen(line));
+	get_next_line(0, &line);
+	dim = ft_strtok(line, ':');
+	ft_strtok(dim, ' ');
+	map->height = ft_atoi(ft_strtok(NULL, ' '));
+	map->width = ft_atoi(ft_strtok(NULL, ' '));
+	get_next_line(0, &line);
+	free(line);
+	free(dim);
+}
+
+void	get_player(t_map *map)
+{
+	char	*line;
+	char	*tok;
+	int		i;
+
+	line = NULL;
+	tok = NULL;
+	get_next_line(0, &line);
+	tok = ft_strtok(line, ' ');
+	i = 1;
+	while (tok)
+	{
+		if (i == 3)
+			map->letter = tok[1];
+		tok = ft_strtok(NULL, ' ');
+		i++;
+	}
+	free(line);
+}
+
+void	display_pos(int y, int x)
+{
+	printf("%d %d\n", y, x);
+}
+
+void	get_map(t_map *map)
+{
+	char	*line;
+	int		i;
+
+	i = -1;
+	map->map = (char **)malloc(sizeof(char*) * map->height);
+	line = NULL;
+	i = 0;
+	while (i < map->height && get_next_line(0, &line))
+	{
+		line = ft_strchr(line, ' ');
+		map->map[i] = (char *)malloc(sizeof(char) * ft_strlen(ft_strchr(line,
+						' ')));
+		ft_strcpy(map->map[i], ft_strchr(line, ' '));
+		i++;
+	}
+}
+
+void	get_dim_piece(t_piece *piece)
+{
+	char	*line;
+	char	*dim;
+
+	(void)piece;
+	line = NULL;
+	dim = NULL;
+	get_next_line(0, &line);
+	dim = ft_strtok(line, ':');
+	ft_strtok(dim, ' ');
+	piece->height = ft_atoi(ft_strtok(NULL, ' '));
+	piece->width = ft_atoi(ft_strtok(NULL, ' '));
+	free(dim);
+	free(line);
+}
+
+void	get_piece(t_piece *piece)
+{
+	char	*line;
+	int		i;
+
+	i = -1;
+	piece->piece = (char **)malloc(sizeof(char*) * piece->height);
+	line = NULL;
+	i = 0;
+	while (i < piece->height && get_next_line(0, &line))
+	{
+		piece->piece[i] = (char *)malloc(sizeof(char) *
+				ft_strlen(line));
+		ft_strcpy(piece->piece[i], line);
+		i++;
+	}
+}
+
+void	free_all(t_map *map, t_piece *piece)
+{
+	free(map->map);
+	free(piece->piece);
 }
 
 int		main(void)
 {
 	char	*line;
 	t_map	map;
+	t_piece	piece;
+	int		y;
+	int		x;
 
+	(void)piece;
+	x = 1;
+	y = 1;
 	line = NULL;
-	(void)map;
+	get_player(&map); //	OK
 	while (42)
 	{
-		get_next_line(0, &line);
-		write(1, "1 1\n", 4);
+		get_dim_map(&map); //	OK
+		get_map(&map); //		Ok
+		get_dim_piece(&piece);//OK
+		get_piece(&piece);
+		display_pos(map.height, map.width);
+		free_all(&map, &piece);
 	}
 	return (0);
 }
